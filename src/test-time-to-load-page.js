f547655e6da7cacb68abaf0ef9f5b1c7bcf41f6d
vinicius-casarin <vinicius.casarin@kabum.com.br>
2024-04-30 15:34:45 -0300

    generate html report

--- src/test-time-to-load-page.js
+++ src/test-time-to-load-page.js
@@ -1,54 +1,67 @@
-const puppeteer = require("puppeteer");
-const { LOGS_PATH, CUSTOM_UA, TIMEOUT } = require("./constants");
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+const { LOGS_PATH, CUSTOM_UA, TIMEOUT } = require('./constants')
+const lighthouse = (...args) =>
+  import('lighthouse').then(({ default: lighthouse }) => lighthouse(...args))
 
-const iPhone = puppeteer.KnownDevices["iPhone 12"];
+const iPhone = puppeteer.KnownDevices['iPhone 12']
 
 async function getPerformanceTime(page) {
   return JSON.parse(
     await page.evaluate(() => {
-      const [timing] = performance.getEntriesByType("navigation");
+      const [timing] = performance.getEntriesByType('navigation')
       return JSON.stringify({
-        format: timing ? "PerformanceNavigationTiming" : "PerformanceTiming",
+        format: timing ? 'PerformanceNavigationTiming' : 'PerformanceTiming',
         data: timing || window.performance.timing,
-      });
+      })
     })
-  );
+  )
 }
 
-async function loadPage(pageSetup, deviceName, pageURL, title, index) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+async function loadPage(
+  pageSetup,
+  deviceName,
+  pageURL,
+  title,
+  index,
+  folderName
+) {
+  const filePath = `${LOGS_PATH}/${folderName}/${title}-${deviceName}-${index}`
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
 
-  page.setDefaultTimeout(TIMEOUT);
+  page.setDefaultTimeout(TIMEOUT)
 
-  const pageReady = await pageSetup(page);
+  const pageReady = await pageSetup(page)
+
+  await lighthouseAudit(pageURL, pageReady, filePath)
   await pageReady.tracing.start({
-    path: `${LOGS_PATH}/${title}-${deviceName}-${index}.json`,
+    path: `${filePath}.json`,
     screenshots: true,
-  });
+  })
 
   try {
-    await pageReady.goto(pageURL);
-    const times = await getPerformanceTime(pageReady);
+    await pageReady.goto(pageURL)
+    const times = await getPerformanceTime(pageReady)
     console.log(
       `${index};${deviceName};${pageURL};${parseInt(
         times.data.redirectEnd
       )};${parseInt(times.data.requestStart)};${parseInt(
         times.data.responseEnd
       )};${parseInt(times.data.loadEventEnd)}`
-    );
+    )
   } catch (error) {
     console.log(
       `${index};${deviceName};${pageURL};timeout;timeout;timeout;timeout`
-    );
+    )
   } finally {
-    await pageReady.tracing.stop();
-    await browser.close();
+    await pageReady.tracing.stop()
+    await browser.close()
 
-    delete browser;
-    delete page;
-    delete pageReady;
-    delete times;
+    delete browser
+    delete page
+    delete pageReady
+    delete times
   }
 
   // return {
@@ -59,41 +72,76 @@ async function loadPage(pageSetup, deviceName, pageURL, title, index) {
   // }
 }
 
+async function lighthouseAudit(pageURL, pageReady, filePath) {
+  const lighthouseRunner = await lighthouse(
+    pageURL,
+    {
+      output: 'html',
+    },
+    undefined,
+    pageReady
+  )
+
+  fs.writeFileSync(
+    `${filePath}-notas.json`,
+    Object.values(lighthouseRunner.lhr.categories)
+      .map((c) => c.score)
+      .join(', ')
+  )
+  fs.writeFileSync(`${filePath}.html`, lighthouseRunner.report)
+}
+
 async function desktopLarge(page) {
-  await page.setUserAgent(CUSTOM_UA);
+  await page.setUserAgent(CUSTOM_UA)
   await page.setViewport({
     width: 1792,
     height: 999,
-  });
-  return page;
+  })
+  return page
 }
 
 async function mobileIphone(page) {
-  await page.emulate(iPhone);
-  return page;
+  await page.emulate(iPhone)
+  return page
 }
 
-(async () => {
-  const sampling = 1000;
+;(async () => {
+  const sampling = 1
   const pages = [
     {
-      url: "https://stg.kabum.com.br",
-      title: "kabum_home",
+      url: 'https://stg.kabum.com.br',
+      title: 'kabum_home',
     },
-  ];
+  ]
+  const folderName = new Date().getTime()
+  fs.mkdirSync(`${LOGS_PATH}/${folderName}`)
 
   for (let index = 1; index <= sampling; index++) {
     for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-      const page = pages[pageIndex];
+      const page = pages[pageIndex]
 
       try {
-        await loadPage(desktopLarge, "desktop", page.url, page.title, index);
-        await loadPage(mobileIphone, "iphone", page.url, page.title, index);
+        await loadPage(
+          desktopLarge,
+          'desktop',
+          page.url,
+          page.title,
+          index,
+          folderName
+        )
+        await loadPage(
+          mobileIphone,
+          'iphone',
+          page.url,
+          page.title,
+          index,
+          folderName
+        )
       } catch (error) {
-        console.log("erro");
+        console.log('erro', error)
       }
     }
   }
 
-  console.log(`end;end;end;end;end;end;end`);
-})();
+  console.log(`end;end;end;end;end;end;end`)
+})()
